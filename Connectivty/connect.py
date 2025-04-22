f7b26ddb176f63939ba9145b959d7fc7c5a7ed06
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-04-22 20:28:19 +0530

    Compelet mysql connectivity in python

--- Connectivty/connect.py
+++ Connectivty/connect.py
@@ -35,8 +35,25 @@ mycursor=mydb.cursor()
 # print("Records Are inserted")
 
 #Displaying All the Records
-mycursor=mydb.cursor()
-mycursor.execute("Select * from stud")
-result=mycursor.fetchall()
-for i in result:
-    print(i)
+# mycursor=mydb.cursor()
+# mycursor.execute("Select * from stud")
+# result=mycursor.fetchall()
+# for i in result:
+# print(i)
+
+#Updating The Records
+# mycursor=mydb.cursor()
+# update="update stud set Name='Om' where Enroll=3"
+# mycursor.execute(update)
+# print("Record Updated")
+# mydb.commit()
+# mycursor.close()
+# mydb.close()
+
+#Deleting the record
+# mycursor=mydb.cursor()
+# mycursor.execute("Delete from stud where Enroll=3")
+# print("Record Deleted")
+# mydb.commit()
+# mydb.close()
+# mycursor.close()
