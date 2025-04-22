2863a47fd2ef546af48c3a8fe1a6ba8af00027a7
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-04-22 20:16:14 +0530

    connectivty in mysql

--- Connectivty/connect.py
+++ Connectivty/connect.py
@@ -1,1 +1,42 @@
 import mysql.connector
+# mydb = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password=''
+# )
+# print(mydb)
+# mycursor =mydb.cursor()
+# mycursor.execute('Create Database python')
+# mycursor.execute('Show Databases')
+# for i in mycursor:
+#     print(i)
+
+#Creating The Table
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='python'
+)
+mycursor=mydb.cursor()
+# mycursor.execute('Create table stud(Enroll int ,Name varchar(20))')
+# mycursor.execute('show tables')
+# for i in mycursor:
+#     print(i)
+
+
+#Inserting Records
+# insert='insert into stud(Enroll,Name) values(%s,%s)'
+# records=[(1,"Amit"),(2,"Dev"),(3,'Rohan')]
+# mycursor.executemany(insert,records)
+# mydb.commit()
+# mydb.close()
+# mycursor.close()
+# print("Records Are inserted")
+
+#Displaying All the Records
+mycursor=mydb.cursor()
+mycursor.execute("Select * from stud")
+result=mycursor.fetchall()
+for i in result:
+    print(i)
